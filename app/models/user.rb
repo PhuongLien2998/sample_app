82150dec06c07fb095aaf8f38a6ffe23dbe4c6a5
phuong lien <buiphuonglien2907@gmail.com>
2020-09-10 14:23:50 +0700

    chapter12: PASSWORD RESET

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   USERS_PARAMS = %i(name email password password_confirmation).freeze
   VALID_EMAIL_REGEX = Settings.user.email.regex
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   validates :name, presence: true, length: {maximum: Settings.user.name.length}
   validates :email, presence: true,
@@ -55,6 +55,19 @@ class User < ApplicationRecord
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update reset_digest: User.digest(reset_token), reset_sent_at: Time.zone.now
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.user.expired.time.hours.ago
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
 
     resources :users, except: :new
     resources :account_activations, only: :edit
+    resources :password_resets, except: %i(index show destroy)
   end
 end
